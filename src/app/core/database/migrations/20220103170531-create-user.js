fdfd48b0fbf57b63c831f20371d785c86e095999
vohathnhbinh <cryaoticishere@gmail.com>
2022-02-13 13:12:58 +0700

    Reorder migrations

--- src/app/core/database/migrations/20220103170531-create-user.js
+++ src/app/core/database/migrations/20220103170531-create-user.js
@@ -9,9 +9,9 @@ module.exports = {
         defaultValue: Sequelize.UUIDV4,
         allowNull: false
       },
-      fullName: {
+      username: {
         type: Sequelize.STRING,
-        allowNull: false
+        allowNull: true
       },
       email: {
         type: Sequelize.STRING,
@@ -20,6 +20,97 @@ module.exports = {
       password: {
         type: Sequelize.STRING,
         allowNull: false
+      },
+      roleId: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: {
+          model: 'roles',
+          key: 'id'
+        }
+      },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      dateOfBirth: {
+        type: Sequelize.DATE,
+        allowNull: true
+      },
+      nationality: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      avatar: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      mobilePhone: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      gradeId: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: {
+          model: 'grades',
+          key: 'id'
+        }
+      },
+      degreeId: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: {
+          model: 'degrees',
+          key: 'id'
+        }
+      },
+      identityNumber: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      subjects: {
+        type: Sequelize.JSONB,
+        allowNull: true
+      },
+      userToken: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      tokenExpired: {
+        type: Sequelize.DATE,
+        allowNull: true
+      },
+      resetPasswordToken: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      resetPasswordExpired: {
+        type: Sequelize.DATE,
+        allowNull: true
+      },
+      isActive: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: true
+      },
+      isVerified: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+      },
+      isDisabled: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
       },
       createdAt: {
         allowNull: false,
