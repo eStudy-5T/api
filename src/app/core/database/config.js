8f54e794f88b3fa2d6237b7ded611133b4eac01c
tainguyent <taitnguyen@kobiton.com>
2022-02-27 15:25:06 +0700

    Add code to deploy on production

--- src/app/core/database/config.js
+++ src/app/core/database/config.js
@@ -11,6 +11,14 @@ module.exports = {
     seederStorage: 'sequelize'
   },
   production: {
-    url: process.env.DATABASE_URL
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 };
--- src/app/core/database/sequelize.js
+++ src/app/core/database/sequelize.js
@@ -1,14 +1,17 @@
 import Sequelize from 'sequelize';
 
-const sequelize = new Sequelize(
-  process.env.DATABASE_NAME,
-  process.env.DATABASE_USER,
-  process.env.DATABASE_PASSWORD,
-  {
-    host: process.env.DATABASE_HOST,
-    dialect: 'postgres',
-    logging: false
-  }
-);
+const sequelize =
+  process.env === 'production'
+    ? new Sequelize(process.env.DATABASE_URL)
+    : new Sequelize(
+        process.env.DATABASE_NAME,
+        process.env.DATABASE_USER,
+        process.env.DATABASE_PASSWORD,
+        {
+          host: process.env.DATABASE_HOST,
+          dialect: 'postgres',
+          logging: false
+        }
+      );
 
 export default sequelize;
