1964e4d894cb2e7228ea7a0319cf672e1307a35b
tainguyent <81953795+tainguyent@users.noreply.github.com>
2022-03-19 16:55:15 +0700

    Change password API

--- src/app/libs/api-user/user.controller.js
+++ src/app/libs/api-user/user.controller.js
@@ -1,4 +1,14 @@
 import userService from './user.service';
+import get from 'lodash/get';
+import isNil from 'lodash/isNil';
+import User from '../../core/database/models/user';
+
+const USER_PASSWORD_REGEX = new RegExp(
+  '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})'
+);
+const validatePassword = (password, regExp) => {
+  return regExp.test(password);
+};
 
 const userController = {
   getCurrentUser: (req, res) => {
@@ -30,6 +40,74 @@ const userController = {
         console.log(err);
         res.send(err.message);
       });
+  },
+
+  changePassword: async (req, res) => {
+    const userId = get(req, 'user.id');
+    const {
+      currentPassword = '',
+      newPassword = '',
+      confirmPassword = ''
+    } = req.body;
+
+    // Check if passwords is filled
+    if (!currentPassword || !newPassword || !confirmPassword) {
+      return res.status(400).json({
+        message: `Missing required values: [${
+          (!currentPassword ? 'currentPassword ' : '') +
+          (!newPassword ? 'newPassword ' : '') +
+          (!confirmPassword ? 'confirmPassword' : '')
+        }]`
+      });
+    }
+
+    // Check pattern for passwords
+    if (
+      !validatePassword(newPassword, USER_PASSWORD_REGEX) ||
+      !validatePassword(confirmPassword, USER_PASSWORD_REGEX)
+    ) {
+      console.log(
+        !validatePassword(newPassword, USER_PASSWORD_REGEX),
+        !validatePassword(confirmPassword, USER_PASSWORD_REGEX)
+      );
+      return res.status(400).json({
+        message: `Fields not match standard: [${
+          (!validatePassword(newPassword, USER_PASSWORD_REGEX)
+            ? 'newPassword '
+            : '') +
+          (!validatePassword(confirmPassword, USER_PASSWORD_REGEX)
+            ? 'confirmPassword'
+            : '')
+        }]`
+      });
+    }
+
+    // Check newPassword and confirmPassword
+    if (newPassword !== confirmPassword) {
+      return res
+        .status(400)
+        .json({message: 'confirmPassword is not match with newPassword'});
+    }
+
+    // Check currentPassword is match with DB
+    const user = await User.findOne({where: {id: userId}});
+    if (isNil(user) || !user.comparePassword(currentPassword)) {
+      return res.status(400).json({
+        message: 'currentPassword is not correct'
+      });
+    }
+
+    try {
+      await user.update({password: newPassword});
+      req.logout();
+      res
+        .status(200)
+        .clearCookie('access_token')
+        .clearCookie('refresh_token')
+        .send('OK');
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
   }
 };
 
